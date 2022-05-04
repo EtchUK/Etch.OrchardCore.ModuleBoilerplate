91828a18b687a010e460a7b4b98b09760d65e16b
Mehdi Miah <Mehdi.Miah@etchuk.com>
2022-05-04 10:41:59 +0100

    upgrade to orchard core 1.3.0

---
+++ content/Startup.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Data.Migration;
+using OrchardCore.Modules;
+
+namespace Etch.OrchardCore.ModuleBoilerplate
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            // services should be included here.
+        }
+    }
+}
